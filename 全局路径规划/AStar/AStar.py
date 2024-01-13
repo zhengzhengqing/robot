bec112b750e4c5aad8d811d06fe576806a08c042
zhengzhengqing1 <zzq418224@163.com>
2024-01-13 17:25:00 +0800

    还有点小bug

--- 全局路径规划/AStar/AStar.py
+++ 全局路径规划/AStar/AStar.py
@@ -10,17 +10,8 @@ pygame.init()
 
 class AStar:
     def __init__(self):
-        self.current_node = None
-        self.order = 1
-        self.running = True
-        self.open_list = []    # [["13",15]]   13: 表示第1行， 第3列。15表示F值
-        self.closed_list = []  # ["11", "12", "13"]
-        self.g_value_list = {} # {"13": 20}  表示索引为1 和 3 的格子的G值 为20
-        self.path = []
-        self.obstacles = []
-        self.start = []
-        self.end = []
-        self.father_list = {} # {"12": "13"}, 表示索引为1 2 的格子的父节点索引为 1 3
+        
+        self.initialize()
         self.level = 2
         self.width = 1200
         self.hight = 800
@@ -36,20 +27,41 @@ class AStar:
         self.white = (255, 255, 255)
         self.black = (0, 0, 0)
         self.blue = (0, 0, 255)
+        self.green = (0, 255, 0)
+        self.red = (255, 0, 0)
+        self.pink = (255, 0, 255)
 
         # 创建一个表示网格的二维数组
         self.grid = [[self.white for _ in range(self.width // self.grid_size)] for _ in range(self.hight // self.grid_size)]
         self.generate_obstacles(self.level)
         self.thread1 = threading.Thread(target=self.work)
         self.thread1.start()
+
+    def initialize(self):
+        self.order = 1
+        self.running = True
+        self.open_list = []    # [["13","12",15]]  ， 13: 表示行， 12表示列，15表示F值
+        self.closed_list = []  # [{"13":"12"}] ，     13: 表示行， 12表示列
+        self.g_value_list = [] # [["13","12",15]] ，  13: 表示行， 12表示列，15表示G值
+        self.path = []     
+        self.start = []
+        self.end = []
+        self.father_list = [] # [["13","12","13", "15"]] ， 13为行，12为列， 后面两个为父节点的坐标
     
-    def find_neighbors(self, node, g_value):
+    def find_neighbors(self, node): # ["13","12",15]
+        
+        # 获取当前node的g_value
+        g_value =self.get_g_value(node)
+
+        if g_value == None:
+            print("g_value is None")
+            return False
 
         # 字符格式 转成int 格式  “12” -->1 2
         row = int(node[0]) # 行
         col = int(node[1]) # 列
-        neighbors = {}     # 保存临时的相邻方格
-        obstacles = []     # 保存相邻方格中的障碍物
+        neighbors = []     # 保存临时的相邻方格, [["x","y", g ]]
+        obstacles = []     # 保存相邻方格中的障碍物 [["x", "y"]]
 
         """
             查看该索引相邻的8个点, x 为可通行方格， o为障碍物
@@ -66,69 +78,159 @@ class AStar:
                     continue
                 if i == row and j == col:
                     continue
+            
+                if self.is_in_close_list(i, j):
+                    continue
                 
                 if self.is_obstacle(i, j):
-                    obstacles.append(str(i) + str(j))
+                    obstacles.append([str(i) , str(j)])
                     continue
 
+                # 找到目标点
                 if self.is_goal(i, j):
-                            return True 
+                    return True , node
 
                 # 左上角， 右下角， 左下角， 右上角
                 if (i < row and j < col) or (i > row and j > col) or (i > row and j < col) or (i < row and j > col):
-                    neighbors[str(i) + str(j)] = g_value + 14
+                    neighbor = [str(i) , str(j), g_value + 14]
+                    neighbors.append(neighbor)
                 else:
-                    neighbors[str(i) + str(j)] = g_value + 10
+                    neighbor = [str(i) , str(j), g_value + 10]
+                    neighbors.append(neighbor)
 
-        # 遍历障碍物
-        for obstacle in obstacles:
-            if (int(obstacle[0]) == row -1) and (int(obstacle[1]) == col):   # 障碍物在该点的正上方
-                for j in [col -1, col +1]:
-                    index = str(row) + str(j)
-                    if index  in neighbors:
-                        del neighbors[index]
-            elif (int(obstacle[0]) == row +1) and (int(obstacle[1]) == col): # 障碍物在该点的正下方
-                for j in [col -1, col +1]:
-                    index = str(row) + str(j)
-                    if index  in neighbors:
-                        del neighbors[index]
-            elif (int(obstacle[0]) == row) and (int(obstacle[1]) == col -1): # 障碍物在该点的左侧
-                for i in [row -1, row +1]:
-                    index = str(i) + str(col)
-                    if index  in neighbors:
-                        del neighbors[index]
-            elif (int(obstacle[0]) == row) and (int(obstacle[1]) == col +1): # 障碍物在该点的右侧
-                for i in [row -1, row +1]:
-                    index = str(i) + str(col)
-                    if index  in neighbors:
-                        del neighbors[index]
+        # # 遍历障碍物
+        # for obstacle in obstacles:
+        #     if (int(obstacle[0]) == row -1) and (int(obstacle[1]) == col):   # 障碍物在该点的正上方, 把左上角，右上角方格剔除
+        #         for j in [col -1, col +1]:
+        #             # 遍历相邻点
+        #             for index in range(len(neighbors)):
+        #                 if obstacle[0] == index[0] and str(j) == index[1]:
+        #                     del neighbors[index]
+        #     elif (int(obstacle[0]) == row +1) and (int(obstacle[1]) == col): # 障碍物在该点的正下方， 把左右两侧的方格剔除
+        #         for j in [col -1, col +1]:
+        #             # 遍历相邻点
+        #             for index in range(len(neighbors)):
+        #                 if obstacle[0] == index[0] and str(j) == index[1]:
+        #                     del neighbors[index]
+        #     elif (int(obstacle[0]) == row) and (int(obstacle[1]) == col -1): # 障碍物在该点的左侧， 左上角和左下角方格剔除
+        #         for i in [row -1, row +1]:
+        #             # 遍历相邻点
+        #             for index in range(len(neighbors)):
+        #                 if str(i) == index[0] and  obstacle[1] == index[1]:
+        #                     del neighbors[index]
+        #     elif (int(obstacle[0]) == row) and (int(obstacle[1]) == col +1): # 障碍物在该点的右侧， 右上角和右下角方格剔除
+        #         for i in [row -1, row +1]:
+        #             # 遍历相邻点
+        #             for index in range(len(neighbors)):
+        #                 if str(i) == index[0] and  obstacle[1] == index[1]:
+        #                     del neighbors[index]
                 
-            # 判断周围点的有没有更小的G值
-                           
+        # 遍历周围的方格
+        for index in neighbors:
+            result, g_value, num = self.is_in_open_list(index)
 
+            if result: # 已存在, 判断是否有更小的g值
+                if index[2] < g_value:
+                    # 更新F值, 更新g值以及父节点，
+                    self.open_list[num][2] = index[2] + self.get_h_value(index)
+                    self.set_g_value(index)
+                    self.set_father_list(index, node)
+            else: 
+                # 添加到open_list中， 并设置g值和父节点
+                self.set_g_value(index)
+                self.add_in_open_list(index)
+                self.set_father_list(index, node)
+                self.rendering_grid(index) # 渲染方格
+        #print(self.g_value_list)
+        # 对open_list 重新排序
+        self.open_list = sorted(self.open_list, key=lambda x: x[2])
+        #self.set_g_value(index)
+        return False, None
 
-        
+    def is_in_close_list(self, x, y): #[{"13":"12"}]
+        for dictionary in self.closed_list:
+            for key, value in dictionary.items():
+                if key == str(x) and value == str(y):
+                    return True
+        return False
+    
+    def is_in_open_list(self, index):
+        for i in range(len(self.open_list)):
+            if self.open_list[i][0] == index[0] and self.open_list[i][1] == index[1]:
+                g_value = self.get_g_value(index)
+                return True, g_value, i # 返回true 和 g 值,和当前索引
+        return False , None, None
+
+    def get_h_value(self, index):
+        return abs(int(index[0]) - self.end[0])  + abs(int(index[1]) - self.end[1])
+    
+    def set_g_value(self, index):
+        for i in range(len(self.g_value_list)):
+            if self.g_value_list[i][0] == index[0] and self.g_value_list[i][1] == index[1]:
+                self.g_value_list[i][2] = index[2]
+                return
+             
+        self.g_value_list.append([index[0], index[1], index[2]])
+
+    
+    def rendering_grid(self, index):
+        x = int(index[0])
+        y = int(index[1])
+        self.grid[x][y] = self.pink
+
+    def get_str_index(self, index):
+        return [str(index[0]),str(index[1])]
+    
+    def get_g_value(self, index):
+        for tem in self.g_value_list:
+            if index[0] == tem[0] and index[1] == tem[1]:
+                return tem[2]
+        return None
+    
+    def add_in_open_list(self, index):
+        index[2] = index[2] + self.get_h_value(index)
+        self.open_list.append(index)
+    
+    def set_father_list(self, sun, father):
+        for i in range(len(self.father_list)):
+            if self.father_list[i][0] == sun[0] and self.father_list[i][1] == sun[1]:
+                self.father_list[i][2] = father[0]
+                self.father_list[i][3] = father[1]
+                return
+       
+        self.father_list.append([sun[0], sun[1], father[0], father[1]])
 
     def find_paths(self, start):
-        pass
         str_start  = self.get_str_index(start)
-        start_list = []
-        start_list.append(str_start)
-        start_list.append(0)              # 起点的F值暂时设置为0
-        self.g_value_list[str_start] = 0  # 起点时的G值先设置为0
-        self.open_list.append(start_list)
+        str_start.append(0)                  # 起点的F值暂时设置为0, 起点时的G值先设置为0
+        self.g_value_list.append(str_start)  
+        self.open_list.append(str_start)
 
         while len(self.open_list) > 0:
-            if len(self.open_list) == 1:
-                self.current_node = self.open_list[0][0]   # 取出字符串格式的索引值 “13”
-                self.open_list.clear()
-                self.closed_list.append(self.current_node) # 把已经选取过的方格放入到 close_list中
+            tem_dic = {}
+            current_node = self.open_list.pop(0) 
+            tem_dic[current_node[0]] = current_node[1]
+            self.closed_list.append(tem_dic)   
 
-                # 查找相邻的点，将该点的索引和G值传进去
-                self.find_neighbors(self.current_node,  self.g_value_list[self.current_node])  
-       
-    def get_str_index(self, start):
-         return str(start[1]) + str(start[0])
+            # 查找相邻的点，将该点的索引
+            result, goal = self.find_neighbors(current_node)  
+            if result:
+                self.rendering_path(goal)
+                return True
+            
+        return False
+
+    def rendering_path(self, node):
+        while True:
+            if node[0] == str(self.start[0]) and node[1] == str(self.start[1]): # 起点
+                return
+
+            self.grid[int(node[0])][int(node[1])] = self.green
+            for item in self.father_list:
+                if item[0] == node[0] and item[1] == node[1]:
+                    node.clear()
+                    node.append(item[2])
+                    node.append(item[3])
 
     def work(self):
         while(self.running):
@@ -140,7 +242,6 @@ class AStar:
                         return 
                     elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                         
-                        # 鼠标左键点击事件
                         x, y = event.pos
                         grid_x = x // self.grid_size # 列
                         grid_y = y // self.grid_size # 行
@@ -161,12 +262,23 @@ class AStar:
                         self.order +=1
 
             if self.order == 3:
-                self.find_paths(self.start)
-                self.grid = [[self.white for _ in range(self.width // self.grid_size)] for _ in range(self.hight // self.grid_size)]
-                self.generate_obstacles(self.level)
-                self.order = 1  
-                self.start = []
-                self.end = []
+                result = self.find_paths(self.start)
+                if result:
+                    print("成功找到路径")
+                else:
+                    print("无法找到路径")
+
+                jump = False
+                while not jump:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            return 
+                        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:   
+                            self.grid = [[self.white for _ in range(self.width // self.grid_size)] for _ in range(self.hight // self.grid_size)]
+                            self.generate_obstacles(self.level)
+                            self.initialize()
+                            jump = True
+                            
     
     def is_obstacle(self, x, y):
         return self.grid[x][y] == self.black
@@ -200,12 +312,14 @@ class AStar:
         for i in range(num_obstacles):
             obstacle = (random.randint(0, self.rows-1), random.randint(0, self.columns-1))
             self.grid[obstacle[0]][obstacle[1]] = self.black
-            self.obstacles.append(obstacle)
 
     def loop(self):
         # 主循环
         while self.running:
-            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return 
+                
             # 重新绘制栅格
             self.draw_grid()
             time.sleep(0.05)
